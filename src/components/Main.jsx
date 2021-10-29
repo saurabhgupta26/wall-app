082fc15e06ae57faee576e290715798a13d4e982
Saurabh Gupta <saurabhguptaviet@gmail.com>
2021-10-29 10:51:41 +0530

    UI Design

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,7 +5,7 @@ export default function Main() {
   return (
     <div className="main-bg">
       <span className="container text-light bolder h1">People</span>
-      <div className="d-sm-flex justify-content-between align-items-center pt-4  container">
+      <div className="d-sm-flex justify-content-between align-items-center py-4  container">
         {data.lists.map((x) => {
           return (
             <div
@@ -21,7 +21,7 @@ export default function Main() {
 
       <div className="d-sm-flex justify-content-between align-items-start pt-4 container ">
         <div className="d-sm-block sm-margin width_user">
-          <div className="shadow-sm d-block mx-3 my-5 mb-0 pb-0 w-40 rounded-lg smviewb">
+          <div className="shadow-sm d-sm-block mx-3 my-5 mb-0 pb-0 w-40 rounded-lg smviewb">
             {data.users.map((user) => {
               return (
                 <div className="my-4 mt-4 border border-dark rounded-lg p-3 mb-5 height-user">
@@ -31,8 +31,9 @@ export default function Main() {
                       alt="user"
                       className="image_users rounded-lg"
                     />
-                    <div className="d-sm-block mx-2">
+                    <div className="d-block">
                       <span className="text-white h4">{user.name}</span>
+                      <br />
                       <span className="text_id h6">{user.id}</span>
                     </div>
                   </div>
